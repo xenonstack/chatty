8732da93476dbcb9eb5fe2eabe14fe5b580f8bcc
belieflimitless <belieflimitless@gmail.com>
2015-07-13 19:17:16 +0530

    Created tts module.

---
+++ tts/tts.py
@@ -0,0 +1,37 @@
+'''
+Created on 13-Jul-2015
+
+@author: abhay
+'''
+
+from gtts import gTTS
+from pydub import AudioSegment
+
+class TTS():
+    '''
+    Text to speech conversion.
+    '''
+    
+    text = ''
+    lang = 'en'
+
+    def __init__(self):
+        pass
+    
+    def set_language(self, lang):
+        self.lang = lang
+    
+    def load(self, text):
+        self.text = text
+    
+    def mp3(self, path='/tmp/chatty-shout.mp3'):
+        tts = gTTS(text= self.text, lang= self.lang)
+        tts.save(path)
+
+    def wav(self, path='/tmp/chatty-shout.wav'):
+        self.mp3(path + '.mp3')
+        audio_file = AudioSegment.from_mp3(path + '.mp3')
+        audio_file.export(path, format='wav')
+        
+
+''' eof '''
