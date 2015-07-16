64971b5d495cefb3fc29bebbc7dccbc993920749
belieflimitless <belieflimitless@gmail.com>
2015-07-16 15:56:45 +0530

    Added STT

--- speech/stt.py
+++ speech/stt.py
@@ -4,26 +4,31 @@ Created on 13-Jul-2015
 @author: abhay
 '''
 
+import speech_recognition as sr
+
 class STT():
     '''
     Speech audio to text conversion.
     '''
 
+    audio_file = '/tmp/chatty-shout.wav'
+
     def __init__(self):
         pass
     
     def load(self, audio_file= '/tmp/chatty-shout.wav'):
-        fd = open(audio_file, 'rb')
-        audio_binary = ''
-        with fd, audio_binary:
-            try:
-                byte = fd.read(1)
-                while byte != '':
-                    audio_binary += byte
-            except:
-                pass
+        self.audio_file = audio_file
     
     def transcript(self):
-        pass
+        r = sr.Recognizer()
+        with sr.WavFile(self.audio_file) as source:
+            audio = r.record(source)
+        try:
+            list = r.recognize(audio,True)
+            print("Possible transcriptions:")
+            for prediction in list:
+                print(" " + prediction["text"] + " (" + str(prediction["confidence"]*100) + "%)")
+        except LookupError:
+            print("Could not understand audio")
 
 ''' eof '''
